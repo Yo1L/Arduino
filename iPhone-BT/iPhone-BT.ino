10388e4e141cd75d27bd94218fc5d3c80f358a5c
Yoann Le Viavant <yleviavant@gmail.com>
2016-09-03 12:00:49 +0200

    Read/Write commands 0x80 for writing commands and below for reading ones Send distance from wall with 0x8 flag in Centimeters from 0-255 (255 is for infinite) Many little fix and cleaning code

--- iPhone-BT/iPhone-BT.ino
+++ iPhone-BT/iPhone-BT.ino
@@ -15,10 +15,12 @@ MeIR ir;
 int lastDirection;
 int direction;
 bool run = false;
-bool manual = false;
 long lastRun = 0;
-int speed = 80;
+int speed;
+int distance;
 
+#define BLUETOOTH_COMMAND_SIZE  2
+#define REFRESH_COMMAND_DELAY 100 // MSECS
 #define MOTOR_STOP  0
 #define MOTOR_MOVE_FORWARD 1
 #define MOTOR_MOVE_BACKWARD 2
@@ -27,9 +29,10 @@ int speed = 80;
 
 #define BUFFSIZE  4
 
-#define BT_RUN        0x1
-#define BT_SPEED      0x2
-#define BT_DIRECTION  0x4
+#define BT_RUN          0x1
+#define BT_SPEED        0x2
+#define BT_DIRECTION    0x4
+#define BT_RO_DISTANCE  0x8
 
 template<class T> T clamp(T value, T min, T max) {
   if( value < min ) {
@@ -41,12 +44,17 @@ template<class T> T clamp(T value, T min, T max) {
   return value;
 }
 
-void writeBluetoothCommand(const char *data ) {
+void writeBluetoothString(const char *data ) {
   if( !data ) return;
   for( size_t i=0; data[i] != '\0'; i++ ) {
     bt.write(data[i]);
   }
   bt.write('\n');
+}
+
+void sendBluetoothCommand(uint8_t order, int value) {
+  bt.write(order);
+  bt.write(0xff & value);
 }
 
 void toggleRun(bool isRunning) {
@@ -74,7 +82,10 @@ void resetBluetoothBuffer() {
 void readBluetoothCommand() {  
   for( ; bt.available() && bufferPosition<(BUFFSIZE-1); bufferPosition++ ) {
     btBuffer[bufferPosition] = bt.read();
-    
+
+    // 2 bytes
+    // 1st - <128 : write command else >=128 Write command
+    // 2nd - Value
     if( bufferPosition == 1 /*|| btBuffer[bufferPosition] == '\n' && bufferPosition>0*/ ) {
       parseBluetoothCommand(btBuffer, bufferPosition+1);
       resetBluetoothBuffer();
@@ -88,17 +99,24 @@ void readBluetoothCommand() {
 }
 
 void parseBluetoothCommand(char *buff, size_t size) {
-  /*writeBluetoothCommand(commands[0]);
-  writeBluetoothCommand(commands[1]);*/
+  /*writeBluetoothString(commands[0]);
+  writeBluetoothString(commands[1]);*/
 
-  switch(buff[0]) {
+  // convert unsigned to signed int
+  bool write = buff[0] & 0x80 ? true : false;
+  int cmd   = 0xff & (buff[0] & ~0x80);
+  int value = 0xff & buff[1];
+
+  switch(cmd) {
     case BT_RUN: 
       {
         /**
          * run:[0|1]
          */
-        toggleRun(buff[1] == 0 ? false : true);
-        writeBluetoothCommand("run:ok");
+        if( write ) {
+          toggleRun(value == 0 ? false : true);
+        }
+        value = run ? 1 : 0;
       }
       break;
 
@@ -107,10 +125,10 @@ void parseBluetoothCommand(char *buff, size_t size) {
         /**
          * speed:[0-255]
          */
-        int value = 0xff & buff[1]; // convert unsigned to signed int
-        speed = clamp(value, 0, 255);
-    
-        writeBluetoothCommand("speed:ok");
+        if( write ) {
+          speed = clamp(value, 0, 255);
+        }
+        value = speed; // send back the right value
       }
       break;
 
@@ -119,16 +137,30 @@ void parseBluetoothCommand(char *buff, size_t size) {
         /**
          * direction: [0-4]
          */
-        int value = 0xff & buff[1];
-        direction = clamp(value, 0, 4);
-        writeBluetoothCommand("direction:ok");
+        if( write ) {
+          direction = clamp(value, 0, 4);
+        }
+        value = direction; // send back the right value
+      }
+      break;
+
+    case BT_RO_DISTANCE:
+      {
+        /**
+         * READ ONLY Distance from a wall
+         */
+        value = distance > 255 ? 255 : distance;
       }
       break;
       
     default:
-      writeBluetoothCommand("Unknown command");
+      // error
+      cmd = 0xff;
+      value = 0xff;
       break;
   }
+
+  sendBluetoothCommand(cmd, value);
 }
 
 void setup(){
@@ -145,6 +177,10 @@ void setup(){
   Serial.begin(115200);
 
   lastRun = millis();
+
+  lastDirection = direction = MOTOR_STOP;
+  speed = 0;
+  distance = 255;
 
   resetBluetoothBuffer();
 }
@@ -192,13 +228,16 @@ void loop() {
 
   // avoid sleeping (delay)
   long now = millis();
-  if( run && ((lastRun + 100) < now) ) {
+  if( run && ((lastRun + REFRESH_COMMAND_DELAY) < now) ) {
     lastRun = now;
 
-    int distance = ultrasonic_3.distanceCm();
+    distance = ultrasonic_3.distanceCm();
     int go = direction;
     
     if( go == MOTOR_MOVE_FORWARD && distance < 30 ) {
+      // send alert info
+      sendBluetoothCommand(BT_RO_DISTANCE, distance);
+      
       if( distance < 15 ) {
         go = MOTOR_MOVE_BACKWARD;
       }
@@ -212,6 +251,11 @@ void loop() {
     else {
       rgbled_7.setColor(0,0,100,0);
       go = direction;
+    }
+
+    if( go != direction ) {
+      // send info
+      sendBluetoothCommand(BT_DIRECTION, direction);
     }
     
     motor.move(go, speed);
